648feb2cfee5550a290357177454ee7cb14ea972
root <kraval@rarus.ru>
2019-05-24 16:46:05 +0300

    merge

--- bitrix/managed_cache/MYSQL/agents/df/df172012ca59e327a51e5e5d2e4478ff.php
+++ bitrix/managed_cache/MYSQL/agents/df/df172012ca59e327a51e5e5d2e4478ff.php
@@ -1,7 +1,7 @@
 <?
 if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001558702141';
-$dateexpire = '001558705801';
-$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";i:1558702142;}';
+$datecreate = '001558705381';
+$dateexpire = '001558709041';
+$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";i:1558705382;}';
 return true;
 ?>
