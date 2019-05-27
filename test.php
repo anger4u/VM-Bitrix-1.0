a480b97e7da8ed0fe69850c9132227c1eb0ea9a5
root <kraval@rarus.ru>
2019-05-27 07:54:25 +0300

    work fix 07:54

--- test.php
+++ test.php
@@ -1,23 +1,16 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("тест");
-<<<<<<< HEAD
-?><?$APPLICATION->IncludeComponent("bitrix:system.auth.form", "auth", Array(
+?>
+
+
+<?$APPLICATION->IncludeComponent("bitrix:system.auth.form", "auth", Array(
 	"FORGOT_PASSWORD_URL" => "/user/index.php",	// Страница забытого пароля
 		"PROFILE_URL" => "/user/profile.php",	// Страница профиля
 		"REGISTER_URL" => "/user/registration.php",	// Страница регистрации
 		"SHOW_ERRORS" => "N",	// Показывать ошибки
 	),
-	false
-=======
-?><?$APPLICATION->IncludeComponent(
-	"bitrix:main.include",
-	"",
-	Array(
-		"AREA_FILE_RECURSIVE" => "Y",
-		"AREA_FILE_SHOW" => "sect",
-		"AREA_FILE_SUFFIX" => "green_inc",
-		"EDIT_TEMPLATE" => ""
-	)
->>>>>>> 13235046d1ade786905843341bd3962fde739439
-);?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+	false)
+?>
+
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
